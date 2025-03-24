abfbf84d1f1bd55ba3bbfc4d939e2a51b0aad3ec
Tung <sontungnguyen16@gmail.com>
2025-03-24 17:19:34 +0700

    Fix typo

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -5,7 +5,7 @@ const Biography = () => {
     <section id="biography" className="p-5">
       <div className="container">
         <h2>Biography</h2>
-        <p>A young guy try to pursue his dream.</p>
+        <p>A young guy tries to pursue his dream.</p>
       </div>
     </section>
   );
